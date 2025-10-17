d11b5ee276499d664af08f84b48e3ad65d4da648
mahardikakdenie <kucingemas744@gmail.com>
2025-10-17 14:58:42 +0700

    feat:application module and main getting update for deployemnt

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+// src/main.ts
+import { config } from 'dotenv'; // ← tambahkan ini
+config(); // ← muat .env
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ResponseInterceptor } from './common/response/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Hapus properti non-DTO
+      forbidNonWhitelisted: true, // Tolak properti tidak dikenal
+      transform: true, // Transform ke instance class
+    }),
+  );
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  await app.listen(process.env.PORT ?? '');
 }
-bootstrap();
+void bootstrap();
