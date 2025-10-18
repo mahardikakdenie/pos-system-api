5613ae75e5cd695a754291bb7ab1d5f30b976545
mahardikakdenie <kucingemas744@gmail.com>
2025-10-18 22:24:27 +0700

    feat:adjusment code because add getadminclient interface

--- src/supabase/supabase.service.ts
+++ src/supabase/supabase.service.ts
@@ -1,24 +1,33 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 // src/supabase/supabase.service.ts
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
 @Injectable()
-export class SupabaseService implements OnModuleInit {
-  private client: SupabaseClient;
+export class SupabaseService {
+  private supabaseAnon: SupabaseClient;
+  private supabaseAdmin: SupabaseClient;
 
-  onModuleInit() {
-    const url: string = process.env.SUPABASE_URL ?? '';
-    const key: string = process.env.SUPABASE_ANON_KEY ?? '';
+  constructor() {
+    const supabaseUrl = process.env.SUPABASE_URL;
+    const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+    const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-    if (typeof url !== 'string' || typeof key !== 'string' || !url || !key) {
-      throw new Error('SUPABASE_URL dan SUPABASE_ANON_KEY wajib di .env');
+    if (!supabaseUrl || !supabaseAnonKey || !supabaseServiceRoleKey) {
+      throw new Error('Missing Supabase environment variables');
     }
 
-    this.client = createClient(url, key);
+    // Client biasa (untuk auth, query publik)
+    this.supabaseAnon = createClient(supabaseUrl, supabaseAnonKey);
+
+    // Client admin (untuk deleteUser, dll)
+    this.supabaseAdmin = createClient(supabaseUrl, supabaseServiceRoleKey);
   }
 
   getClient(): SupabaseClient {
-    return this.client;
+    return this.supabaseAnon;
+  }
+
+  getAdminClient(): SupabaseClient {
+    return this.supabaseAdmin;
   }
 }
