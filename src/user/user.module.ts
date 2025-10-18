70241d1b68adcaf44d17a7a21d54b44b5af1c2e8
mahardikakdenie <kucingemas744@gmail.com>
2025-10-19 02:07:28 +0700

    feat:added nodemailer service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { SupabaseService } from 'supabase/supabase.service';
+import { MailerService } from 'mailer/mailer.service';
 
 @Module({
-  providers: [UserService, SupabaseService],
+  providers: [UserService, SupabaseService, MailerService],
   controllers: [UserController],
 })
 export class UserModule {}
