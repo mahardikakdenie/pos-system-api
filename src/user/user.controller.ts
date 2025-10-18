43f211d0f092880de74a041b773af59b5fd31781
mahardikakdenie <kucingemas744@gmail.com>
2025-10-18 15:59:34 +0700

    feat:create new routing with authentication for user summaries

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,14 +9,21 @@ import {
 import { AuthGuard } from 'auth/auth.guard';
 import { UserService } from './user.service';
 
-@Controller('api')
+@Controller('api/users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('users')
+  @Get('')
   @UseGuards(AuthGuard)
   @HttpCode(HttpStatus.OK)
   async getAllUsers(@Query('page') page = 1, @Query('limit') limit = 10) {
     return this.userService.getUsers(page, limit);
+  }
+
+  @Get('summaries')
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  async getUserSummary() {
+    return await this.userService.getSummary();
   }
 }
