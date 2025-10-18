49dae8bc9412a1fc7c1829ace912c8b311512d42
mahardikakdenie <kucingemas744@gmail.com>
2025-10-18 15:58:55 +0700

    feat:add logic for service get all user for summaries

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -15,7 +15,8 @@ export class UserService {
     const { count, error: countError } = await this.supabaseService
       .getClient()
       .from('profiles')
-      .select('*', { count: 'exact', head: false });
+      .select('*', { count: 'exact', head: false })
+      .neq('role_id', 0);
 
     if (countError) throw new Error(countError.message);
 
@@ -23,16 +24,50 @@ export class UserService {
     const { data, error } = await this.supabaseService
       .getClient()
       .from('profiles')
-      .select('*')
+      .select(
+        `
+        id,
+        email,
+        role_id,
+        status,
+        role:roles!inner(
+          id,
+          name,
+          descriptions
+        )
+      `,
+      )
+      .neq('role_id', 0)
       .range(offset, offset + limit - 1);
 
     if (error) throw new Error(error.message);
 
     return {
-        limit,
-        page,
-        data: data,
-        total: count as number,
+      limit,
+      page,
+      data: data,
+      total: count as number,
+    };
+  }
+
+  async getSummary(): Promise<{
+    all: number;
+  }> {
+    try {
+      // Get All data user counter
+      const { count: all, error: countError } = await this.supabaseService
+        .getClient()
+        .from('profiles')
+        .select('*', { count: 'exact', head: false })
+        .neq('role_id', 0);
+
+        if (countError) throw new Error(countError.message);
+
+        return {
+          all: all ?? 0,
+        }
+    } catch (error) {
+      throw new Error(error);
     }
   }
 }
