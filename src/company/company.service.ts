900828d7c18005409bc0c8ef83c97053091472b7
mahardikakdenie <kucingemas744@gmail.com>
2025-10-21 04:26:30 +0700

    feat:companies function service

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { BadGatewayException, Injectable } from '@nestjs/common';
+import { SupabaseService } from 'supabase/supabase.service';
 
 @Injectable()
-export class CompanyService {}
+export class CompanyService {
+    constructor(private readonly supabaseService: SupabaseService) {}
+
+    async getDataCompanies() {
+        try {
+            const {} = await this.supabaseService.getClient().from('companies').select("*");
+        } catch (error) {
+            throw new BadGatewayException(error);
+        }
+    }
+}
