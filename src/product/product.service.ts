cb09e3b2fbbeeb96508c33ef812bdf1915a8ca71
mahardikakdenie <kucingemas744@gmail.com>
2025-10-21 01:10:20 +0700

    feat:update product service

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,38 +1,70 @@
-import { BadGatewayException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadGatewayException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { SupabaseService } from 'supabase/supabase.service';
 import { CreateProductDTO } from './product.dto';
 
 @Injectable()
 export class ProductService {
-    constructor(private readonly supabaseService:SupabaseService) {}
+  constructor(private readonly supabaseService: SupabaseService) {}
 
-    async getDataProducts() {
-        try {
-            const {data, error} = await this.supabaseService.getClient().from('products').select('*');
+  async getDataProducts() {
+    try {
+      const { data, error } = await this.supabaseService
+        .getClient()
+        .from('products')
+        .select('*');
 
-            if (error) {
-                throw new BadGatewayException({message: error.message, name: error.name});
-            }
+      if (error) {
+        throw new BadGatewayException({
+          message: error.message,
+          name: error.name,
+        });
+      }
 
-            return data
-        } catch (error) {
-            throw new BadGatewayException(error);
-        }
+      return data;
+    } catch (error) {
+      throw new BadGatewayException(error);
     }
+  }
 
-    async createProduct(productPayload: CreateProductDTO) {
-        try {
-            const {data, error} = await this.supabaseService.getClient().from('products').insert(productPayload);
+  async createProduct(productPayload: CreateProductDTO) {
+    try {
+      const { data, error } = await this.supabaseService
+        .getClient()
+        .from('products')
+        .insert(productPayload);
 
-            if (error) {
-                throw new BadGatewayException({message: error.message, name: error.name});
-            }
+      if (error) {
+        throw new BadGatewayException({
+          message: error.message,
+          name: error.name,
+        });
+      }
 
-            return {
-                data,
-            }
-        } catch (error) {
-            throw new BadGatewayException(error);
-        }
+      return {
+        data,
+      };
+    } catch (error) {
+      throw new BadGatewayException(error);
     }
+  }
+
+  async updateProduct(productPayload: CreateProductDTO, productId: string) {
+    try {
+      const { data, error } = await this.supabaseService
+        .getClient()
+        .from('products')
+        .update(productPayload)
+        .eq('id', productId);
+
+        if (error) throw new BadGatewayException({message: error.message});
+
+        return data;
+    } catch (error) {
+      throw new BadGatewayException(error);
+    }
+  }
 }
