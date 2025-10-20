ab1a9d94062686214c588b0e7c36a244ffbcb7ea
mahardikakdenie <kucingemas744@gmail.com>
2025-10-21 04:14:09 +0700

    feat:added supabase service as provider at company module

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CompanyService } from './company.service';
 import { CompanyController } from './company.controller';
+import { SupabaseService } from 'supabase/supabase.service';
 
 @Module({
-  providers: [CompanyService],
+  providers: [CompanyService, SupabaseService],
   controllers: [CompanyController]
 })
 export class CompanyModule {}
