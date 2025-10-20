f6e4700268ebd4ba027a3013df7ea61acfaad4c2
mahardikakdenie <kucingemas744@gmail.com>
2025-10-21 01:06:05 +0700

    feat:init company module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { AuthService } from './auth/auth.service';
 import { AuthController } from './auth/auth.controllers';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ProductModule],
+  imports: [AuthModule, UserModule, ProductModule, CompanyModule],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
 })
