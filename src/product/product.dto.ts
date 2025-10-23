71a397295bbc1ace46e9b374c51cd0984da85488
mahardikakdenie <kucingemas744@gmail.com>
2025-10-23 10:43:22 +0700

    feat:product DTO Update Product DTO

--- src/product/product.dto.ts
+++ src/product/product.dto.ts
@@ -5,6 +5,7 @@ import {
   isNotEmpty,
   IsNotEmpty,
   IsObject,
+  IsOptional,
   isString,
   IsString,
 } from 'class-validator';
@@ -41,5 +42,37 @@ export class CreateProductDTO {
   
   @IsObject({ message: 'Logs harus berupa objek JSON' })
   @IsNotEmpty({ message: 'Logs wajib diisi' })
-  logs: Record<string, any>; // atau ganti dengan interface spesifik
+  logs: Record<string, any>;
+}
+
+export class UpdateProduct {
+  @IsString()
+  @IsOptional({ message: 'Email wajib diisi' })
+  name: string;
+
+  @IsString()
+  @IsOptional({ message: 'Description wajib diisi' })
+  description: string;
+
+  @IsString()
+  @IsOptional({ message: 'Description wajib diisi' })
+  slug: string;
+
+  @IsString()
+  @IsOptional({ message: 'Image wajib diisi' })
+  image: string;
+
+  @IsString()
+  @IsOptional({message: "Price is required"})
+  price: string;
+
+  @IsEnum(UserStatus, {
+    message: 'Status harus salah satu dari: active, inactive, suspend',
+  })
+  @IsOptional({ message: 'Status wajib diisi' })
+  status: UserStatus;
+  
+  @IsObject({ message: 'Logs harus berupa objek JSON' })
+  @IsOptional({ message: 'Logs wajib diisi' })
+  logs: Record<string, any>;
 }
