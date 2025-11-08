e3ad4c8217c179797c87757c3fbc1ee577071b7c
mahardikakdenie <kucingemas744@gmail.com>
2025-11-08 16:54:15 +0700

    fix:remove theme module duplicate at main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,6 @@ import { ProductModule } from './product/product.module';
 import { CompanyModule } from './company/company.module';
 import { SupabaseService } from 'supabase/supabase.service';
 import { ThemeModule } from './theme/theme.module';
-import { ThemeModule } from './theme/theme.module';
 
 @Module({
   imports: [AuthModule, UserModule, ProductModule, CompanyModule, ThemeModule],
