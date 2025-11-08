68da826cce9dee807154c874c86f44184e298c7f
mahardikakdenie <kucingemas744@gmail.com>
2025-11-08 17:05:14 +0700

    chore:added optional field or keys for product DTO

--- src/product/product.dto.ts
+++ src/product/product.dto.ts
@@ -1,4 +1,5 @@
 // src/auth/dto/login.dto.ts
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsEnum,
@@ -19,30 +20,44 @@ export enum UserStatus {
 
 export class CreateProductDTO {
   @IsString()
+  @ApiProperty({example: 'Wireless Headphones'})
   @IsNotEmpty({ message: 'Email wajib diisi' })
   name: string;
 
   @IsString()
+  @ApiProperty({example: 'High-quality wireless headphones with noise cancellation.'})
   @IsNotEmpty({ message: 'Description wajib diisi' })
   description: string;
 
   @IsString()
+  @ApiProperty({example: 'wireless-headphones'})
   @IsNotEmpty({ message: 'Image wajib diisi' })
   image: string;
 
   @IsString()
+  @ApiProperty({example: '89.99'})
   @IsNotEmpty({message: "Price is required"})
   price: string;
 
   @IsEnum(UserStatus, {
     message: 'Status harus salah satu dari: active, inactive, suspend',
   })
+  @ApiProperty({example: 'inactive'})
   @IsNotEmpty({ message: 'Status wajib diisi' })
   status: UserStatus;
   
   @IsObject({ message: 'Logs harus berupa objek JSON' })
+  @ApiProperty({example: { createdBy: 'admin', createdAt: '2025-04-01T10:00:00Z' }})
   @IsNotEmpty({ message: 'Logs wajib diisi' })
   logs: Record<string, any>;
+
+  @IsString()
+  @IsOptional()
+  categoryId: string;
+
+  @IsString()
+  @IsOptional()
+  slug: string;
 }
 
 export class UpdateProduct {
