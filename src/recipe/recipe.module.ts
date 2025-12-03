c1b2eae64cc2699ddf0a8461188fb2d1610557b1
mahardikakdenie <kucingemas744@gmail.com>
2025-12-03 15:40:18 +0700

    chore:assign supabaseclient provider that service use in recipe module

--- src/recipe/recipe.module.ts
+++ src/recipe/recipe.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RecipeController } from './recipe.controller';
 import { RecipeService } from './recipe.service';
+import { SupabaseService } from 'supabase/supabase.service';
 
 @Module({
   controllers: [RecipeController],
-  providers: [RecipeService]
+  providers: [RecipeService, SupabaseService]
 })
 export class RecipeModule {}
