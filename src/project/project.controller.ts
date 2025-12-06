ae64d24f0576a3924b06938e311dfaf616f90818
mahardikakdenie <kucingemas744@gmail.com>
2025-12-06 21:18:10 +0700

    feat(project-controller): create endpoint for create new data projects

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
-import { ApiOkResponse } from '@nestjs/swagger';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { ApiBody, ApiOkResponse } from '@nestjs/swagger';
 import { AuthGuard } from 'auth/auth.guard';
-import { ProjectListResponseDto } from './project.dto';
+import { ProjectDTO, ProjectListResponseDto } from './project.dto';
+import { ProjectService } from './project.service';
 
 @Controller('api/project')
 export class ProjectController {
+    constructor(private readonly projectService: ProjectService) {}
 
     @Get('')
     @UseGuards(AuthGuard)
@@ -12,5 +14,14 @@ export class ProjectController {
     @ApiOkResponse({ type: ProjectListResponseDto })
     async getDataProjects() {
         return await this.getDataProjects();
+    }
+    
+    @Post('')
+    @UseGuards(AuthGuard)
+    @HttpCode(HttpStatus.CREATED)
+    @ApiOkResponse({type: ProjectListResponseDto})
+    @ApiBody({ type: ProjectDTO })
+    async createProject(@Body() projectPayload: ProjectDTO) {
+        return await this.projectService.createProject(projectPayload);
     }
 }
