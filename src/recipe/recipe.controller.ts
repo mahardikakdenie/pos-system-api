f86fdc3ecc43f5c4d906baca43158b0f8de5e151
mahardikakdenie <kucingemas744@gmail.com>
2025-12-06 17:55:51 +0700

    feat(recipe-controller): getData recipe

--- src/recipe/recipe.controller.ts
+++ src/recipe/recipe.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
+import { RecipeDTO } from './recipe.dto';
+import { RecipeService } from './recipe.service';
+import { AuthGuard } from 'auth/auth.guard';
 
-@Controller('recipe')
-export class RecipeController {}
+@Controller('/api/recipe')
+export class RecipeController {
+    constructor(private readonly recipeService: RecipeService) {}
+    
+    @Get('')
+    @ApiOkResponse({
+        type: RecipeDTO
+    })
+    @UseGuards(AuthGuard)
+    async getData() {
+        return this.recipeService.getDataRecipe();
+    }
+}
