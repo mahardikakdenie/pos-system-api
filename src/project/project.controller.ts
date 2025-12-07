6334bb47417fac24a095ba1f2568856dc41679ed
mahardikakdenie <kucingemas744@gmail.com>
2025-12-08 06:32:14 +0700

    feat(project-controller): update api params for documentation update project and create new endpoint for delete data

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,12 +1,12 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Put, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiBody, ApiOkResponse } from '@nestjs/swagger';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiOkResponse, ApiParam } from '@nestjs/swagger';
 import { AuthGuard } from 'auth/auth.guard';
 import { ProjectDTO, ProjectListResponseDto } from './project.dto';
 import { ProjectService } from './project.service';
 
 @Controller('api/project')
 export class ProjectController {
-    constructor(private readonly projectService: ProjectService) {}
+    constructor(private readonly projectService: ProjectService) { }
 
     @Get('')
     @UseGuards(AuthGuard)
@@ -16,12 +16,12 @@ export class ProjectController {
     async getDataProjects() {
         return await this.projectService.getDataProjects();
     }
-    
+
     @Post('')
     @UseGuards(AuthGuard)
     @ApiBearerAuth()
     @HttpCode(HttpStatus.CREATED)
-    @ApiOkResponse({type: ProjectListResponseDto})
+    @ApiOkResponse({ type: ProjectListResponseDto })
     @ApiBody({ type: ProjectDTO })
     async createProject(@Body() projectPayload: ProjectDTO) {
         return await this.projectService.createProject(projectPayload);
@@ -31,9 +31,30 @@ export class ProjectController {
     @UseGuards(AuthGuard)
     @ApiBearerAuth()
     @HttpCode(HttpStatus.OK)
-    @ApiOkResponse({type: ProjectListResponseDto})
-    @ApiBody({type: ProjectDTO})
+    @ApiOkResponse({ type: ProjectListResponseDto })
+    @ApiBody({ type: ProjectDTO, })
+    @ApiParam({
+        name: 'id',
+        type: Number,
+        description: 'Numeric ID of the project to update',
+        example: 123,
+    })
     async updateProject(@Param() projectId: number, @Body() projectPayload: ProjectDTO) {
         return await this.projectService.updateProjects(projectPayload, projectId);
+    }
+
+    @Delete(':id')
+    @UseGuards(AuthGuard)
+    @ApiBearerAuth()
+    @HttpCode(HttpStatus.OK)
+    @ApiOkResponse({ type: ProjectListResponseDto })
+    @ApiParam({
+        name: 'id',
+        type: Number,
+        description: 'Numeric Id of the project to Delete',
+        example: 1,
+    })
+    async deleteProject(@Param() projectId: number) {
+        return await this.projectService.deleteProjects(projectId);
     }
 }
