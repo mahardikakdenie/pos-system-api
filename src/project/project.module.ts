6d62d0bfcedcc67e4638dc7c5135e631ab946a6e
mahardikakdenie <kucingemas744@gmail.com>
2025-12-13 22:06:36 +0700

    feat(skill): create new module projects skill

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { SupabaseService } from 'supabase/supabase.service';
 import { ProjectController } from './project.controller';
+import { SkillService } from './skill/skill.service';
+import { SkillController } from './skill/skill.controller';
 
 @Module({
-  providers: [ProjectService, SupabaseService],
-  controllers: [ProjectController]
+  providers: [ProjectService, SupabaseService, SkillService],
+  controllers: [ProjectController, SkillController]
 })
 export class ProjectModule {}
