1ef6eb1e79d5a41e8c3e773ebba1e3a38eac0fe3
mahardikakdenie <kucingemas744@gmail.com>
2025-12-13 22:33:28 +0700

    feat(projects-controller): create new endpoint for fetching current user projects

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -5,72 +5,92 @@ import { ProjectDTO, ProjectListResponseDto } from './project.dto';
 import { ProjectService } from './project.service';
 import { RoleGuard } from 'auth/role.guard.guard';
 import { RequireRole } from 'common/common.decorator';
+import { RequireProfile } from 'common/decorator/require-profile.decorator';
 
 @Controller('api/project')
 export class ProjectController {
-    constructor(private readonly projectService: ProjectService) { }
+  constructor(private readonly projectService: ProjectService) {}
 
-    @Get('')
-    @UseGuards(AuthGuard, RoleGuard)
-    @RequireRole('admin', 'superadmin')
-    @ApiBearerAuth()
-    @HttpCode(HttpStatus.OK)
-    @ApiOkResponse({ type: ProjectListResponseDto })
-    @ApiQuery({ name: 'page', required: false, type: Number, example: 1 })
-    @ApiQuery({ name: 'limit', required: false, type: Number, example: 10 })
-    @ApiQuery({
-        name: 'entities',
-        required: false,
-        type: String,
-        example: 'resumes(id,name),tags(id,name)',
-        description: 'Comma-separated list of relation fields. Example: resumes(id,name)'
-    })
-    async getDataProjects(
-        @Query('page') page: number = 1,
-        @Query('limit') limit: number = 5,
-        @Query('entities') entities: string = ''
-    ) {
-        return await this.projectService.getDataProjects(page, limit, entities);
-    }
+  @Get('')
+  @UseGuards(AuthGuard, RoleGuard)
+  @RequireRole('admin', 'superadmin')
+  @ApiBearerAuth()
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ type: ProjectListResponseDto })
+  @ApiQuery({ name: 'page', required: false, type: Number, example: 1 })
+  @ApiQuery({ name: 'limit', required: false, type: Number, example: 10 })
+  @ApiQuery({
+    name: 'entities',
+    required: false,
+    type: String,
+    example: 'resumes(id,name)',
+    description: 'Comma-separated list of relation fields. Example: resumes(id,name)',
+  })
+  async getDataProjects(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 5,
+    @Query('entities') entities: string = '',
+  ) {
+    return await this.projectService.getDataProjects(page, limit, entities);
+  }
 
-    @Post('')
-    @UseGuards(AuthGuard)
-    @ApiBearerAuth()
-    @HttpCode(HttpStatus.CREATED)
-    @ApiOkResponse({ type: ProjectListResponseDto })
-    @ApiBody({ type: ProjectDTO })
-    async createProject(@Body() projectPayload: ProjectDTO) {
-        return await this.projectService.createProject(projectPayload);
-    }
+  @Post('')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOkResponse({ type: ProjectListResponseDto })
+  @ApiBody({ type: ProjectDTO })
+  async createProject(@Body() projectPayload: ProjectDTO) {
+    return await this.projectService.createProject(projectPayload);
+  }
 
-    @Put(':id')
-    @UseGuards(AuthGuard)
-    @ApiBearerAuth()
-    @HttpCode(HttpStatus.OK)
-    @ApiOkResponse({ type: ProjectListResponseDto })
-    @ApiBody({ type: ProjectDTO, })
-    @ApiParam({
-        name: 'id',
-        type: Number,
-        description: 'Numeric ID of the project to update',
-        example: 123,
-    })
-    async updateProject(@Param() projectId: number, @Body() projectPayload: ProjectDTO) {
-        return await this.projectService.updateProjects(projectPayload, projectId);
-    }
+  @Put(':id')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ type: ProjectListResponseDto })
+  @ApiBody({ type: ProjectDTO })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    description: 'Numeric ID of the project to update',
+    example: 123,
+  })
+  async updateProject(@Param('id') projectId: number, @Body() projectPayload: ProjectDTO) {
+    return await this.projectService.updateProjects(projectPayload, projectId);
+  }
 
-    @Delete(':id')
-    @UseGuards(AuthGuard)
-    @ApiBearerAuth()
-    @HttpCode(HttpStatus.OK)
-    @ApiOkResponse({ type: ProjectListResponseDto })
-    @ApiParam({
-        name: 'id',
-        type: Number,
-        description: 'Numeric Id of the project to Delete',
-        example: 1,
-    })
-    async deleteProject(@Param() projectId: number) {
-        return await this.projectService.deleteProjects(projectId);
-    }
+  @Delete(':id')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ type: ProjectListResponseDto })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    description: 'Numeric ID of the project to delete',
+    example: 1,
+  })
+  async deleteProject(@Param('id') projectId: number) {
+    return await this.projectService.deleteProjects(projectId);
+  }
+
+  @Get('my-projects')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ type: ProjectListResponseDto })
+  @ApiQuery({
+    name: 'entities',
+    required: false,
+    type: String,
+    example: 'resumes(id,name,profiles(id))',
+    description: 'Comma-separated relation fields. Example: resumes(id,name,profiles(id))',
+  })
+  async getDataMyProjects(
+    @RequireProfile() profile: any,
+    @Query('entities') entities: string = '',
+  ) {
+    return await this.projectService.getMyProjects(profile, entities);
+  }
 }
