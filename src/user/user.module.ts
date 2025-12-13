d26cc4b3dc90992a82a59a0503789325f990c5b7
mahardikakdenie <kucingemas744@gmail.com>
2025-12-13 19:35:07 +0700

    feat(user): added new module roles

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,11 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { SupabaseService } from 'supabase/supabase.service';
 import { MailerService } from 'mailer/mailer.service';
+import { RoleService } from './role/role.service';
+import { RoleController } from './role/role.controller';
 
 @Module({
-  providers: [UserService, SupabaseService, MailerService],
-  controllers: [UserController],
+  providers: [UserService, SupabaseService, MailerService, RoleService],
+  controllers: [UserController, RoleController],
 })
 export class UserModule {}
