c74091d6b92b2f52430fc3084c3accc0572fbb70
mahardikakdenie <kucingemas744@gmail.com>
2025-12-13 19:33:10 +0700

    chore(helpers): update a logic for selected strings entities data

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -2,7 +2,8 @@ export const generateSlug = (input: string): string => {
   return input.toLowerCase().replace(/[^a-z0-9]/g, '-');
 };
 
-export const entities = (PROJECT_SCHEME: string = '*', fields: string[]): string => {
+export const entities = (PROJECT_SCHEME: string = '*', selectedEntities: string): string => {
+  const fields = selectedEntities ? selectedEntities.split(',').map(e => e.trim()) : [];
   if (fields.length === 0) {
     return PROJECT_SCHEME;
   }
