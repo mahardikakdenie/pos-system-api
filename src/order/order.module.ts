b261de4d5f824b87e0c81f0d40eff80a4a804811
mahardikakdenie <kucingemas744@gmail.com>
2025-12-25 17:41:06 +0700

    feat(order-module): register new module tenant

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { SupabaseService } from 'supabase/supabase.service';
+import { TenantService } from './tenant/tenant.service';
 
 @Module({
-  providers: [OrderService, SupabaseService],
+  providers: [OrderService, SupabaseService, TenantService],
   controllers: [OrderController]
 })
 export class OrderModule {}
