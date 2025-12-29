dcfc0f018f636752203cb17097d777199c5d3d39
mahardikakdenie <kucingemas744@gmail.com>
2025-12-29 14:24:39 +0700

    feat(company-stats): initial service for fetching statistic of company

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CompanyService } from './company.service';
 import { CompanyController } from './company.controller';
 import { SupabaseService } from 'supabase/supabase.service';
+import { StatsService } from './stats/stats.service';
 
 @Module({
-  providers: [CompanyService, SupabaseService],
+  providers: [CompanyService, SupabaseService, StatsService],
   controllers: [CompanyController]
 })
 export class CompanyModule {}
