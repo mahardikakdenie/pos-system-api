1ee1ac8e0663a42af8659b56048ce81dd3fc1761
mahardikakdenie <kucingemas744@gmail.com>
2025-12-29 09:08:19 +0700

    feat(company-service): added params for adjusment the data

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,46 +1,55 @@
 import { BadGatewayException, Injectable } from '@nestjs/common';
 import { SupabaseService } from 'supabase/supabase.service';
 import { CompanyDTO } from './company.dto';
+import { entities } from 'common/helpers';
 
 @Injectable()
 export class CompanyService {
-    constructor(private readonly supabaseService: SupabaseService) {}
+  constructor(private readonly supabaseService: SupabaseService) {}
 
-    async getDataCompanies() {
-        try {
-            const { data, error } = await this.supabaseService.getClient().from('companies').select("*");
+  async getDataCompanies(selectedEntities: string = '', orderBy: string = 'id') {
+    try {
+      const { data, error } = await this.supabaseService
+        .getClient()
+        .from('companies')
+        .select(entities('*', selectedEntities))
+        .order(orderBy);
 
-            if (error) throw new BadGatewayException({message: error.message});
+      if (error) throw new BadGatewayException({ message: error.message });
 
-            return data;
-        } catch (error) {
-            throw new BadGatewayException(error);
-        }
+      return data;
+    } catch (error) {
+      throw new BadGatewayException(error);
     }
+  }
 
-    async createCompanyData(companyPayload: CompanyDTO) {
-        try {
-            const {data, error} = await this.supabaseService.getClient().from('companies').insert({
-                name: companyPayload.name,
-                slug: this.generateSlug(companyPayload.slug),
-                email: companyPayload.email,
-                account_url: companyPayload.account_url,
-                status: companyPayload.status || 'requested',
-            }).select('*');
+  async createCompanyData(companyPayload: CompanyDTO) {
+    try {
+      const { data, error } = await this.supabaseService
+        .getClient()
+        .from('companies')
+        .insert({
+          name: companyPayload.name,
+          slug: this.generateSlug(companyPayload.slug),
+          email: companyPayload.email,
+          account_url: companyPayload.account_url,
+          status: companyPayload.status || 'requested',
+        })
+        .select('*');
 
-            if (error) throw new BadGatewayException({message: error.message});
+      if (error) throw new BadGatewayException({ message: error.message });
 
-            return data;
-        } catch (error) {
-            throw new BadGatewayException(error);
-        }
+      return data;
+    } catch (error) {
+      throw new BadGatewayException(error);
     }
+  }
 
-    // create generate slug
-    private generateSlug(name: string): string {
-        return name
-            .toLowerCase()
-            .replace(/ /g, '-')
-            .replace(/[^\w-]+/g, '');
-    }
+  // create generate slug
+  private generateSlug(name: string): string {
+    return name
+      .toLowerCase()
+      .replace(/ /g, '-')
+      .replace(/[^\w-]+/g, '');
+  }
 }
