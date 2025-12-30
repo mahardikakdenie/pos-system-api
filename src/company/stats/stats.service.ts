c3d29855e6b6fe4e5ba5888c0ea419039c3b826a
mahardikakdenie <kucingemas744@gmail.com>
2025-12-30 17:01:54 +0700

    feat(company-stast): create new functionality service

--- src/company/stats/stats.service.ts
+++ src/company/stats/stats.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { BadGatewayException, Injectable } from '@nestjs/common';
+import { SupabaseService } from 'supabase/supabase.service';
 
 @Injectable()
-export class StatsService {}
+export class StatsService {
+    constructor(private readonly supabaseService: SupabaseService) {}
+
+    async getSummary() {
+        try {
+            const {count, error: countError} = await this.supabaseService.getClient().from('companies')
+                .select("*", {count: 'exact'});
+
+                return {
+                    count,
+                };
+        } catch (error) {
+            throw new BadGatewayException(error);
+        }
+    }
+}
