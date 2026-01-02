6b565564b48ac5ec565ab4a38be53a004bda092c
mahardikakdenie <kucingemas744@gmail.com>
2026-01-02 17:20:33 +0700

    feat(company): register new module company stats for show the statistic companies

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -3,9 +3,10 @@ import { CompanyService } from './company.service';
 import { CompanyController } from './company.controller';
 import { SupabaseService } from 'supabase/supabase.service';
 import { StatsService } from './stats/stats.service';
+import { CompanyStatsController } from './stats/stats.controller';
 
 @Module({
   providers: [CompanyService, SupabaseService, StatsService],
-  controllers: [CompanyController]
+  controllers: [CompanyController, CompanyStatsController]
 })
 export class CompanyModule {}
